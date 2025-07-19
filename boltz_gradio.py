8c45eb3acdd5df8a828ba0835604e57365c88cf4
Me <yushiang420@gmail.com>
2025-07-19 12:27:10 +0800

    fix

--- boltz_gradio.py
+++ boltz_gradio.py
@@ -1077,7 +1077,8 @@ def update_result_visualization(name: str, rank_name: str, name_rank_f_map: dict
                     length_split.append(int(last_res) if last_res != '.' else i)
                     i = 0
                 if last_mdl_num is not None and mdl_num != last_mdl_num:
-                    length_split.append(int(last_res) if last_res != '.' else i)
+                    if len(length_split) == 1:
+                        length_split.append(int(last_res) if last_res != '.' else i)
                     break
                 last_c = c
                 last_res = res_id
@@ -1353,7 +1354,8 @@ def update_vhts_result_visualization(name_fpth_map: dict, evt: gr.SelectData):
                     length_split.append(int(last_res) if last_res != '.' else i)
                     i = 0
                 if last_mdl_num is not None and mdl_num != last_mdl_num:
-                    length_split.append(int(last_res) if last_res != '.' else i)
+                    if len(length_split) == 1:
+                        length_split.append(int(last_res) if last_res != '.' else i)
                     break
                 last_c = c
                 last_res = res_id
